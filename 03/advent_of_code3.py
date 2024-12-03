64b2e779e6846327a53846898e1b761336bfb612
kvinkleris <kvinkleris@gmail.com>
2024-12-04 00:40:12 +0200

    full advent of code day 3

--- 03/advent_of_code3.py
+++ 03/advent_of_code3.py
@@ -10,7 +10,18 @@ text = ''.join([i if ord(i) < 128 else ' ' for i in string])
 number_pattern = r"""\d+"""
 answers = re.findall(pattern, text)
 answ = 0
-for item in answers:
-    numbers = re.findall(number_pattern, item)
-    answ += int(numbers[0]) * int(numbers[1])
+
+do_pattern = r"""do\(\)"""
+dont_pattern = r"""don't\(\)"""
+
+do_pattern_split = (re.split(do_pattern, text))
+
+for item in do_pattern_split:
+    dont_patterns = re.split(dont_pattern, item)
+    answers = re.findall(pattern, dont_patterns[0])
+    for item in answers:
+        numbers = re.findall(number_pattern, item)
+        answ += int(numbers[0]) * int(numbers[1])
+
+
 print(answ)
