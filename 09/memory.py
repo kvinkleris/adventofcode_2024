8297dd66d3728256a11b4f2bc9d0c1a41db5cc1b
kvinkleris <kvinkleris@gmail.com>
2024-12-12 23:00:02 +0200

    advent of code day 9 part 2 failure

--- 09/memory.py
+++ 09/memory.py
@@ -44,32 +44,61 @@ def format_first_method():
 
 def insert_into_list(num_to_insert, count_to_insert, index_to_remove):
     """Insert blocks into memory """
-    for index, item in enumerate(dict_of_dots):
-        block_size = 0
-        if (abs(int(last_item) - int(item)) == 1):
-            block_size += 1
-        else:
-            block_size = 0
-        if block_size + 1 >= count_to_insert:
-            memory_list[index:index+block_size] = count_to_insert
-            memory_list[index_to_remove:index_to_remove+block_size] = '.'
-            break
-        last_item = item
 
-        
+    for index_i, ele in enumerate(memory_list):
+        if index_i % 1000 == 0:
+            print(f"Calculating... {index_i}/{len(memory_list)}")
+        if ele == '.':
+            space_to_fill = 0
+            for index_j, ele_j in enumerate(memory_list[index_i:]):
+                if ele_j == '.':
+                    space_to_fill += 1
+                else:
+                    break
+            if space_to_fill >= count_to_insert:
+
+                if (index_i < index_to_remove):
+                    #print(f"index i {index_i}")
+                    #print(f"insert into list {num_to_insert} count to insert {count_to_insert} index_to_remove {index_to_remove} space to fill {space_to_fill}")
+                    if (count_to_insert > 1):
+                        memory_list[index_i:index_i + count_to_insert] = [num_to_insert] * count_to_insert
+                        memory_list[index_to_remove: index_to_remove + count_to_insert + 1] = ['.'] * count_to_insert
+                    else:
+                        memory_list[index_i] = num_to_insert
+                        memory_list[index_to_remove] = '.'
+                    #print(memory_list)
+                    space_to_fill = 0
+                break
         
 def format_second_method():
     """Format memory string by second method"""
-    old_element = '.'
-    for index_j, _ in enumerate(memory_list):
-        if memory_list[-(index_j + 1)] != '.':
-            if old_element != memory_list[-(index_j + 1)]:
-                old_element = memory_list[-(index_j + 1)]
-                block_counter = 0
-            block_counter += 1
+    dict_of_blocks = {}
+    max_value = 0
+    for index_j, item in enumerate(memory_list):
+        if index_j % 1000 == 0:
+            print(f"formatting  {index_j}/{len(memory_list)}")
+        if item != '.':
+            if int(item) > max_value:
+                max_value = int(item)
+        if item not in dict_of_blocks and item != '.':
+            dict_of_blocks[item] = {"elem_num" : item, "block_size" : 1, "starting_index" : index_j}
+        elif item in dict_of_blocks:
+            dict_of_blocks[item]["block_size"] = dict_of_blocks[item]["block_size"] + 1
+        else:
+            continue
 
-            print(f"{old_element} {block_counter}")
-    
+    #print(dict_of_blocks)
+
+
+
+
+    for value in range (max_value, 0, -1):
+        if value in dict_of_blocks:
+            #print(value)
+            insert_into_list(dict_of_blocks[value]["elem_num"], dict_of_blocks[value]["block_size"], dict_of_blocks[value]["starting_index"])
+
+    #print(memory_list)
+
 
 formated_file = open("formated_memory.txt", "w")
 
