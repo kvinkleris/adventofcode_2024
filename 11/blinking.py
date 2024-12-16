5e9be9f22933ab81a3b3f3a105bff2c3d1db1ed9
kvinkleris <kvinkleris@gmail.com>
2024-12-16 23:01:05 +0200

    advent of code day 11

--- 11/blinking.py
+++ 11/blinking.py
@@ -1,40 +1,36 @@
+from collections import defaultdict
+
 file = open("blinking.txt", "r")
 
 data = [int(x) for x in file.read().split(" ")]
-print(data)
 
+nums = defaultdict(int)
+for x in data:
+    nums[x] += 1
 counter = 0
-new_data = []
 
 
 map_of_answers = {}
-def recursive_search(depth, answ, i, og):
-    if depth == 75:
-        return answ
+def build_answer(nums: dict):
+    print(nums)
+    new_nums = defaultdict(int)
 
-    if (answ[i] == 0):
-        answ[i] = 1
-        recursive_search(depth+1,answ, i, answ[i])
-    elif len(str(answ[i])) % 2 == 0:
-        temp = answ[i]
-        answ[i] = int(temp // (10 ** ((len(str(temp))) // 2)))
-        answ.insert(i+1, int(temp % ((10 ** ((len(str(temp))) // 2)))))
-        recursive_search(depth+1,answ, i + 1, answ[i+1])
-        recursive_search(depth+1,answ, i, answ[i])
-        
-    else:
-        answ[i] = answ[i] * 2024
-        recursive_search(depth+1, answ, i, answ[i])
-    return answ
+    for x in nums:
+        if x == 0:
+            new_nums[x + 1] +=  nums[0]
+        elif len(str(x)) % 2 == 0:
+            new_nums[int(x // (10 ** (len(str(x)) / 2)))] += nums[x]
+            new_nums[int(x % (10 ** (len(str(x)) / 2)))] += nums[x]
+        else:
+            new_nums[x*2024] += nums[x]
+    return new_nums
 
-
-print(data)
-answ = []
-for i in data:
-    answ.append(recursive_search(0, [i], 0))
 final_answ = 0
-for item in answ:
-    final_answ += len(item)
 
+while counter < 75:
+    nums = build_answer(nums)
+    counter += 1
     
+for elem in nums:
+    final_answ += nums[elem]
 print(final_answ)
