01a374795959a0519f55aabaa3fe950119acce12
kvinkleris <kvinkleris@gmail.com>
2025-01-05 00:58:49 +0200

    advent of code day 19

--- 19/linen_input.py
+++ 19/linen_input.py
@@ -13,28 +13,28 @@ def read_files():
 
 
 DESIGNS, COMBOS = read_files()
-
-@cache
-def DFS(curr_word, goal_word, answ, prev_word):
+DP = {}
+def DFS(goal_word):
     """Find all possible word combos"""
-    curr_word = curr_word.replace(" ", "")
-
-    if (len(curr_word) > 0) and curr_word != goal_word[:len(curr_word)]:
-        #print("return 0")
-        return 0
-    if curr_word == goal_word:
-        #print("return 1")
-        return 1
+    if goal_word in DP:
+        return DP[goal_word]
     answ = 0
+    if len(goal_word) == 0:
+        return 1
+   
+    
     for design in DESIGNS:
-        answ += DFS(curr_word + design, goal_word, answ, curr_word)
-
-
+        design = design.replace(" ", "")
+        if goal_word[:len(design)] == design:
+            answ += DFS(goal_word[len(design):])
+  
+    DP[goal_word] = answ
     return answ
 
 
 final_answ = 0
-for combo in COMBOS:
-    final_answ += DFS("", combo, 0, "")
 
+for combo in COMBOS:
+    final_answ += DFS(combo)
+print(DP)
 print(final_answ)
