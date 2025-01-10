bed1b48af22e03303ca825205a61137664ddb908
kvinkleris <kvinkleris@gmail.com>
2025-01-11 01:02:57 +0200

    advent of code day 20 failing bad

--- 20/maze.py
+++ 20/maze.py
@@ -1,8 +1,8 @@
 """Race condition advent of code day 2024"""
 import heapq
+from collections import Counter
 file = open("maze.txt", "r")
 map_data = [list(x) for x in file.read().split("\n")]
-print(map_data)
 START = tuple()
 END = tuple()
 for i, row in enumerate(map_data):
@@ -14,17 +14,19 @@ for i, row in enumerate(map_data):
 
 
 Q = []
+
 def search_map(maze_map):
     SEEN = []
+    DIST = {}
     DIRS = [(-1, 0), (0, 1), (1,0), (0, -1)]
     dist = 0
     best_dist = None
-    heapq.heappush(Q, (START[0], START[1], DIRS[0], 0))
-    heapq.heappush(Q, (START[0], START[1], DIRS[1], 0))
-    heapq.heappush(Q, (START[0], START[1], DIRS[2], 0))
-    heapq.heappush(Q, (START[0], START[1], DIRS[3], 0))
+    heapq.heappush(Q, (START[0], START[1], DIRS[0], 0, [(START[0], START[1])]))
+    heapq.heappush(Q, (START[0], START[1], DIRS[1], 0, [(START[0], START[1])]))
+    heapq.heappush(Q, (START[0], START[1], DIRS[2], 0, [(START[0], START[1])]))
+    heapq.heappush(Q, (START[0], START[1], DIRS[3], 0, [(START[0], START[1])]))
     while len(Q) != 0:
-        y_coords, x_coords, direction, dist = heapq.heappop(Q)
+        y_coords, x_coords, direction, dist, PATH = heapq.heappop(Q)
 
         y_coords += direction[0]
         x_coords += direction[1]
@@ -36,31 +38,28 @@ def search_map(maze_map):
 
         if maze_map[y_coords][x_coords] == '#':
             continue
-        if maze_map[y_coords][x_coords] == '1':
-            cheat_activated_coords_y = y_coords
-            cheat_activated_coords_x = x_coords
 
-        if maze_map[y_coords][x_coords] == '2':
-            cheat_activated_coords_y_two = y_coords
-            cheat_activated_coords_x_two = x_coords
+        if (y_coords, x_coords, direction) not in DIST:
+            DIST[(y_coords, x_coords, direction)] = dist
 
             
 
         if maze_map[y_coords][x_coords] == 'E' and best_dist is None:
             SEEN.append((y_coords, x_coords, direction))
             best_dist = dist + 1
-            return best_dist, SEEN
+            return best_dist, PATH
         elif maze_map[y_coords][x_coords] == 'E' and dist is not None and dist < best_dist:
             best_dist = dist + 1
         if (y_coords, x_coords, direction) in SEEN:
             continue
         else:
             SEEN.append((y_coords, x_coords, direction))
-        heapq.heappush(Q, (y_coords, x_coords, DIRS[0], dist + 1))
-        heapq.heappush(Q, (y_coords, x_coords, DIRS[1], dist + 1))
-        heapq.heappush(Q, (y_coords, x_coords, DIRS[2], dist + 1))
-        heapq.heappush(Q, (y_coords, x_coords, DIRS[3], dist + 1))
-    return best_dist, SEEN
+        NEW_PATH = PATH + [(y_coords, x_coords)]    
+        heapq.heappush(Q, (y_coords, x_coords, DIRS[0], dist + 1, NEW_PATH))
+        heapq.heappush(Q, (y_coords, x_coords, DIRS[1], dist + 1, NEW_PATH))
+        heapq.heappush(Q, (y_coords, x_coords, DIRS[2], dist + 1, NEW_PATH))
+        heapq.heappush(Q, (y_coords, x_coords, DIRS[3], dist + 1, NEW_PATH))
+    return best_dist
 
 list_of_maps = []
 removed_walls_set = set()
@@ -74,28 +73,27 @@ for i, row in enumerate(map_data[:-1]):
                 removed_walls_set.add(((i,j), (i+1, j)))
             removed_walls_set.add((i,j))
 
-base_best_dist, PATH = search_map(map_data)
-
-NEW_PATH = []
-for step in PATH:
-    NEW_PATH.append((step[0], step[1]))
-
-speedup_list = []
-for step in NEW_PATH:
-    if (step[0] + 2, step[1]) in NEW_PATH:
-        START = (step[0] + 2, step[1])
-        new_temp, _ = search_map(map_data)
-
-    if (step[0] + 2, step[1]) in NEW_PATH:
-        START = (step[0] + 2, step[1])
-        new_temp, _ = search_map(map_data)
+base_best_dist, BEST_PATH = search_map(map_data)
 
 
-
-    
-
-
-    
-                
-            
-
+def cheats(track, max_dist):
+    """Calculate possible speedups using cheats"""
+    global START
+    speedup_counter = Counter()
+    for t1, tuple_value in enumerate(track):
+        y1 = tuple_value[0]
+        x1 = tuple_value[1]
+        for t2 in range(t1 + 3, len(track)):
+            y2, x2 = track[t2]
+            dist = abs(x2 - x1) + abs(y2 - y1)
+            if dist <= max_dist and t2 - t1 > dist:
+                speedup_counter[t2 - t1 + dist] += 1
+    return speedup_counter
+speed_counter = cheats(BEST_PATH, 2)
+final_answ = 0
+print(speed_counter)
+for key in speed_counter:
+    if key >= 100:
+        final_answ += speed_counter[key]
+        
+print(final_answ)
