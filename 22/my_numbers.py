d8a92b9f501919b663172a829cf6c1e86cb25285
unknown <kvinkleris@gmail.com>
2025-09-01 23:22:26 +0300

    advent of code day 22 complete

--- 22/my_numbers.py
+++ 22/my_numbers.py
@@ -22,52 +22,48 @@ def evolve(secret_num):
     secret_num = prune(secret_num)
     return secret_num
 
-# my_answ = 0
-# for item in numbers:
-#      my_num = int(item)
-#      for i in range(0, 2000):
-#          my_num = evolve(my_num)
-#      my_answ += my_num
-# my_num = 123
+def generate_sequences(seqs):
+    sequences = set()
+    for seq in seqs:
+        for x in range(0, len(seq) - 3):
+            sequences.add(tuple((seq[x][1],seq[x+1][1], seq[x+2][1], seq[x+3][1])))
+            #print(my_tuple)
+            #sequences.add(my_tuple)
+    return sequences
 
-# print(my_answ)
-
-seqs = []
+hash_map = {}
 my_answ = 0
-possible_sequences = {} 
+all_seqs = set()
 for item in numbers:
-  
-    nums = []
+    hash_map[item] = {}
+    seq = []
     my_num = int(item)
     old_num = my_num % 10
     #print(old_num)
+    sliding_window = []
     for i in range(0, 2000):
-        old_num = my_num % 10
+        old_num = my_num
         my_num = evolve(my_num)
-        nums.append(old_num - my_num % 10)
-    already_seen = False
-    for i in range (0, len(nums) - 3):
-        if not item in possible_sequences:
-            possible_sequences[item] = {}
-        if not (nums[i], nums[i+1],nums[i+2], nums[i+3]) in possible_sequences[item]:
-            possible_sequences[item] = possible_sequences[item] | { (nums[i], nums[i+1],nums[i+2], nums[i+3]) : nums[i+3]}
-        elif possible_sequences[item][(nums[i], nums[i+1],nums[i+2], nums[i+3])] in possible_sequences[item]:
-            possible_sequences[item][(nums[i], nums[i+1],nums[i+2], nums[i+3])] += nums[i+3]
-    #print(seq)
-print(possible_sequences)
+        sliding_window.append(my_num % 10 - old_num % 10)
+        if len(sliding_window) == 4:
+            all_seqs.add(tuple(sliding_window))
+            if tuple(sliding_window) not in hash_map[item]:
+                hash_map[item] |= {tuple(sliding_window) : my_num % 10}
+            sliding_window.pop(0)
+        if len(sliding_window) > 4:
+            print("FATAL ERROR")
+            quit()
 
 best = 0
-#print(set_of_sequences)
+best_seq = tuple()
+for sequence in all_seqs:
+    my_sum = 0
+    for h_map_key in hash_map:
+        if sequence in hash_map[h_map_key]:
+            my_sum += hash_map[h_map_key][sequence]
+    if my_sum > best:
+        best_seq = sequence
+        best = my_sum
 
+print(best_seq)
 print(best)
-#print(seqs)
-# for sequence in set_of_sequences:
-#     curr_money = 0
-#     for seq in seqs:
-#         for i in range(0, len(seq) - 3):
-#             if sequence == tuple((seq[i][1], seq[i+1][1], seq[i+2][1], seq[i+3][1])):
-#                 curr_money += seq[i+3][0] % 10
-#                 break
-#if best < curr_money:
- #       best = curr_money
-#print(best)
